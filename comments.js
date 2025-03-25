7182510f6102c602bfbf33454ee71d9a63781b29
Vinicius Porto Almeida <v.almeida1284@gmail.com>
2025-03-25 13:04:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-// Cria o servidor
+// Create web server
 const server = http.createServer((req, res) => {
   res.statusCode = 200; 
   res.setHeader('Content-Type', 'text/plain'); 
